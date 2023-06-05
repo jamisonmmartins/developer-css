8fc5a4926d1de5ce8eac858fdf6c2ebe08a9307b
Jamison M. Martins <99983207+jamisonmmartins@users.noreply.github.com>
2023-06-05 17:48:19 -0300

    correção da funçao

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let botaoMenu = querySelector('#menu')
 let menuAberto = 'false'
 
-function AbrirOuFecharMenu() {
-    if (menuAberto = false) {
+function AbrirOuFecharMenu(menuAberto) {
+    if (menuAberto === false) {
         bataoMenu.innerText = 'Fechar'
     }
 }
 
 
-botaoMenu.onclick = AbrirOuFecharMenu
+botaoMenu.onclick = AbrirOuFecharMenu()
