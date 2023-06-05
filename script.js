9356afb28b341dc03523b62dbb2b04843ccddf24
Jamison M. Martins <99983207+jamisonmmartins@users.noreply.github.com>
2023-06-05 17:45:38 -0300

    create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let botaoMenu = querySelector('#menu')
+let menuAberto = 'false'
+
+function AbrirOuFecharMenu() {
+    if (menuAberto = false) {
+        bataoMenu.innerText = 'Fechar'
+    }
+}
+
+
+botaoMenu.onclick = AbrirOuFecharMenu
