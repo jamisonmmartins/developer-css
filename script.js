e1a76dac4b89f93de0faca585f469416e3099c26
Jamison M. Martins <martins7284@gmail.com>
2023-06-06 22:00:21 -0300

    update

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ let menuAberto = false
 function AbrirOuFecharMenu() {
     switch (menuAberto) {
         case false:
+            document.querySelector('#menu p').innerText = 'Fechar'
             menuAberto = true
             menuMobile.classList.add('menu-mobile')
             break;
         case true:
+            document.querySelector('#menu p').innerText = 'menu'
             menuAberto = false
             menuMobile.classList.remove('menu-mobile')
             break;
